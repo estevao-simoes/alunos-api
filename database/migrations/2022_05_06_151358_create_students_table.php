e66b9d0be2126627fbfef42f0cab65d452858f79
Estevão Simães <estevaosimoes@MacBook-Air-de-Estevao.local>
2022-05-06 12:38:57 -0300

    creating model and migration

---
+++ database/migrations/2022_05_06_151358_create_students_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('students', function (Blueprint $table) {
+            $table->id()->startingValue(101235);
+            
+            $table->string('name');
+            $table->string('email');
+            $table->string('cpf');
+            
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('students');
+    }
+};
