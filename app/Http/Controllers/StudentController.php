ca15f1869dedcd9027ad30472fa99f6e4d7698f6
Estevão Simães <estevaosimoes@MacBook-Air-de-Estevao.local>
2022-05-06 13:02:56 -0300

    adding crud functions to student api controller

---
+++ app/Http/Controllers/StudentController.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Student;
+use Illuminate\Http\Request;
+
+class StudentController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $students = Student::all();
+
+        return response()->json($students, 200);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $this->validate($request, [
+            'name' => 'required|string',
+            'email' => 'required|email',
+            'cpf' => 'required|cpf'
+        ]);
+
+        $newStudent = Student::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'cpf' => $request->cpf
+        ]);
+
+        return response()->json($newStudent, 201);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Student  $student
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Student $student)
+    {
+        return response()->json($student, 200);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Student  $student
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Student $student)
+    {
+        $this->validate($request, [
+            'name' => 'string',
+            'email' => 'email'
+        ]);
+
+        $updatedStudent = $student->update([
+            'name' => $request->name,
+            'email' => $request->email
+        ]);
+
+        return response()->json($updatedStudent, 204);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Student  $student
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Student $student)
+    {
+        return response()->json($student->delete());
+    }
+}
